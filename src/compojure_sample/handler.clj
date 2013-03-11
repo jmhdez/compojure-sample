e0c1d78d5505d7b6e91c3bc784490315b8abd6d8
Juan M. Hernández <juan.hernandez.arroyo@gmail.com>
2013-03-11 20:55:51 +0100

    extracted view to its own file

--- src/compojure_sample/handler.clj
+++ src/compojure_sample/handler.clj
@@ -1,35 +1,11 @@
 (ns compojure-sample.handler
   (:use compojure.core)
-  (:use hiccup.page)
-  (:use hiccup.element)
   (:require [compojure.handler :as handler]
-            [compojure.route :as route]))
-
-(defn the-page []
-  (h/html5 
-    [:head
-	  [:title "Mega Facts"]]
-	[:body
-	  [:div#container
-	    [:div#header
-		  [:h1 "Mega Facts"]]
-		[:div#content
-		  [:div#left-column.column
-		    [:h2 "Elige tu héroe"]
-			[:ul#heroes
-			  [:li.hero-name
-			    (link-to "#" "Chuck Norris")]]]
-		  [:div#right-column.column
-		    [:h2 "Facts"]
-			[:ul#facts]
-			[:label {:for "new-fact"} "Añadir nuevo fact:"]
-			[:textarea#new-fact]
-			[:a#add-new-fact {:href "#"} "Añadir"]]]
-		[:div#footer
-		  [:a {:href "http://blog.koalite.com"}]]]]))
+            [compojure.route :as route]
+			[compojure-sample.views :as view]))
 
 (defroutes app-routes
-  (GET "/" [] (the-page))
+  (GET "/" [] (view/index))
   (route/files "/public" {:root "public"})
   (route/not-found "Not Found"))
 
---
+++ src/compojure_sample/views.clj
@@ -0,0 +1,26 @@
+(ns compojure-sample.views
+  (:use hiccup.page)
+  (:use hiccup.element))
+			
+(defn index []
+  (html5 
+    [:head
+	  [:title "Mega Facts"]]
+	[:body
+	  [:div#container
+	    [:div#header
+		  [:h1 "Mega Facts"]]
+		[:div#content
+		  [:div#left-column.column
+		    [:h2 "Elige tu héroe"]
+			[:ul#heroes
+			  [:li.hero-name
+			    (link-to "#" "Chuck Norris")]]]
+		  [:div#right-column.column
+		    [:h2 "Facts"]
+			[:ul#facts]
+			[:label {:for "new-fact"} "Añadir nuevo fact:"]
+			[:textarea#new-fact]
+			[:a#add-new-fact {:href "#"} "Añadir"]]]
+		[:div#footer
+		  [:a {:href "http://blog.koalite.com"}]]]]))
