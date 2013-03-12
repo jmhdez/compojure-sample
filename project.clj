3fe3a287e8dbda553747589fb9231a797472004f
Juan M. Hernández <juan.hernandez.arroyo@gmail.com>
2013-03-12 21:33:14 +0100

    Updated project info

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject compojure-sample "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Koalite's Compojure Tutorial"
+  :url "http://blog.koalite.com/2013/03/tutorial-compojure-i-creando-una-aplicacion-web-funcional/"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [compojure "1.1.5"]
 				 [hiccup "1.0.2"]]
