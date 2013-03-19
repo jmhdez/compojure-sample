e1804333bcbc6c74734588a4c2ab8df0f41f19fc
Juan M. Hernández <juan.hernandez.arroyo@gmail.com>
2013-03-19 07:56:45 +0100

    applied fix from @jneira to add-fact!

--- src/compojure_sample/model.clj
+++ src/compojure_sample/model.clj
@@ -19,10 +19,7 @@
 	(get @heroes hero))
 
 (defn add-fact! [hero fact]
-	(swap! heroes 
-	  (fn [h]
-		(let [facts (get h hero)]
-		  (assoc h hero (conj facts fact))))))
+	(swap! heroes update-in [hero] conj fact))
 		  
 (defn get-heroes []
   (sort (keys @heroes)))
